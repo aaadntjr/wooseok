851aff7075872b1a353ed2e121cfb43aa1758199
aaadntjr <38950467+aaadntjr@users.noreply.github.com>
2018-05-04 22:27:29 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,14 +21,29 @@ $(document).ready(function(){
         setInterval(function(){
             
             $img.each(function(){
-             $(this).css("left", $(this).position().left-1000);   
+                 $(this).css("left", $(this).position().left-1000);   
             });
+            
+            
+            
             $first = $("#banner"+first);
             $last = $("#banner"+last);
             
             
             if($first.position().left < 0) {
-                $first.css(             "left",$last.position().left);
+                
+                // 다시 오른쪽으로 보내려는 엘리먼트를 일단 투명하게 만든다.
+                $first.css("opacity", "0");
+                
+                $first.css("left",$last.position().left);
+                
+                // 내가 작업했던 녀석을 기억하기 위해서 따로 저장해둔다.
+                var $dd = $first;
+                
+                setTimeout(function() {
+                    // 2초 뒤에 내가 아까 opacity 를 0으로 만들었던 녀석을 원상복구 시킨다.
+                    $dd.css("opacity", "1");
+                }, 2000);
                              
                 first++;
                 last++;
