319175e75f277ba95a08ec3ee9b9da669c337dd7
Choi woo seok <38950467+aaadntjr@users.noreply.github.com>
2018-05-05 19:47:19 +0900

    제발좀 되라

--- js/main.js
+++ js/main.js
@@ -1,66 +1,69 @@
-$(document).ready(function () {
-
- 
-
-    var count = $('.top ul li').length;
-
-    var img_width = $('.top ul li img').css('width');
-
-    var ul_width = count * parseInt(img_width);
-
-    $('.top ul').css('width', ul_width + "px");
-
- 
-
-    var list = $('.top ul li');
-
-    var topleft = 0;
-
-    list.each(function () {
-
-        $(this).css('left', topleft);
-
-        topleft += $(this).width();
-
+$(document).ready(function(){
+    
+    //배너 광고 열기
+    $(".wrap section .top_right button").click(function(){
+       $("body .ad").removeClass("btn_close"); 
     });
-
- 
-
- 
-
-    setInterval(function () {
-
-        list.each(function () {
-
-            var left_pos =parseInt($(this).css('left'));
-
-            $(this).css('left', left_pos - 1000);
-
-            if(left_pos==-1000){
-
-                $(this).css({
-
-                    left:'1000px',
-
-                    opacity:0
-
-                });
-
-            }else {
-
-                $(this).css('opacity','1');
-
-            };
-
-        });
-
+    
+    //배너 광고 닫기
+    $(".ad button").click(function(){
+       $("body .ad").addClass("btn_close"); 
+    });
+    
+    //서브메뉴 드롭다운
+    
+    $(".wrap .main li").hover(function(){
+        $("ul",this).slideToggle();
+    });
+    
+    //슬라이드 쇼
+    
+    var count = $('.wrap .visual ul li').length;
+    var img_width = $('.wrap .visual ul li img').css('width');
+    var ul_width = count * parseInt(img_width);
+    
+    $('.wrap .visual ul').css('width',ul_width+"px");
+    
+    var list = $('.wrap .visual ul li')
+    var visual_left = 0;
+    
+    list.each(function(){
+       $(this).css('left',visual_left);
+        visual_left += $(this).width();
+    });
+    
+    setInterval(function(){
        
+        list.each(function(){
+           var left_pos = parseInt($(this).css('left'));
+            
+            $(this).css('left',left_pos - 1000);
+            
+            if(left_pos==-1000){
+                $(this).css({left:'1000px'});
+                $(this).children().css({opacity:'0'});
+            } else {
+                $(this).children().css({opacity:'1'});
+            }
+        });
+    },2000);
+   
+    
+});//다큐멘트 레디 펑션
 
-    }, 2100);
+//start();
+ //var imgs =2;
+ //var now =0;
+/*
+function start() {
+    $(".imgs>img").eq(0).siblings().css({"margin-left":"-2000px"});
+    setInterval(function(){slide();},2000);
+}
 
- 
 
- 
-
-});
-
+function slide() {
+    now = now ==imgs ?0 : now +=1;
+    $(".imgs>img").eq(now-1).css({"margin-left":"-2000px"});
+    $(".imgs>img").eq(now).css({"margin-left":"0px"});
+}
+*/
