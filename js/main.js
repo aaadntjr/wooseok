ea5cee209ee6965f431be41b06d71b1652d9aedb
Choi woo seok <38950467+aaadntjr@users.noreply.github.com>
2018-05-05 11:52:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
  
-    var count = $('.top ul li').length;
-    var img_width = $('.top ul li img').css('width');
+    var count = $(".top ul li").length;
+    var img_width = $('.top ul li img').css("width");
     var ul_width = count * parseInt(img_width);
-    $('.top ul').css('width', ul_width + "px");
+    $(".top ul").css("width", ul_width + "px");
  
-    var list = $('.top ul li');
+    var list = $(".top ul li");
     var topleft = 0;
     list.each(function () {
-        $(this).css('left', topleft);
+        $(this).css("left", topleft);
         topleft += $(this).width();
     });
  
  
     setInterval(function () {
         list.each(function () {
-            var left_pos =parseInt($(this).css('left'));
-            $(this).css('left', left_pos - 1000);
+            var left_pos =parseInt($(this).css("left"));
+            $(this).css("left", left_pos - 1000);
             if(left_pos==-1000){
                 $(this).css({
                     left:'1000px',
                     opacity:0
                 });
             }else {
-                $(this).css('opacity','1');
+                $(this).css("opacity","1");
             }
         });
        
